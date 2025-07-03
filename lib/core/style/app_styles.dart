9cc21d9d6bff0cf51fd2a921ff1cd638c99a1b0e
Mohamed Ali <ma8510007@gmail.com>
2025-07-03 18:13:28 +0300

    Refactor text styles in AppStyles for consistency and clarity

--- lib/core/style/app_styles.dart
+++ lib/core/style/app_styles.dart
@@ -11,21 +11,52 @@ class AppStyles {
     fontFamily: 'Inter',
   );
 
-  static const TextStyle headline2 = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.w600,
-    fontFamily: fontFamily,
-  );
-
-  static const TextStyle bodyText1 = TextStyle(
+  static const TextStyle font16Weight800Blue = TextStyle(
     fontSize: 16,
-    fontWeight: FontWeight.normal,
+    fontWeight: FontWeight.w800,
+    color: ColorsManger.blueColor,
     fontFamily: fontFamily,
   );
 
-  static const TextStyle bodyText2 = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.normal,
+  static const TextStyle font9Weight800Blue = TextStyle(
+    fontSize: 9,
+    fontWeight: FontWeight.w500,
+    color: ColorsManger.blueColor,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle font9Weight800 = TextStyle(
+    fontSize: 9,
+    fontWeight: FontWeight.w500,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle font9Weight800Yellow = TextStyle(
+    fontSize: 9,
+    fontWeight: FontWeight.w500,
+    color: ColorsManger.yellowColor,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle font12Weight800White = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w800,
+    color: ColorsManger.whiteColor,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle font12Wieght800 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w800,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle font16Weight500 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle font10Weight600 = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w600,
     fontFamily: fontFamily,
   );
 }
